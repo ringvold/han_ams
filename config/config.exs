2549b74f2158cb7d324a66bc878845e897b034f9
Harald Ringvold <harald.ringvold@gmail.com>
2019-08-01 22:25:52 +0200

    Change public keys to be added

--- config/config.exs
+++ config/config.exs
@@ -31,8 +31,7 @@ config :logger, backends: [RingLogger]
 keys =
   [
     Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
+    Path.join[(System.user_home!(), ".ssh", "id_rsa_mac.pub"]),
   ]
   |> Enum.filter(&File.exists?/1)
 
